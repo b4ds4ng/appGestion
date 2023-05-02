2db6f6de4ab5473677088084fec56731c97f0cd1
b4ds4ng <andreu.pastor@telefonica.net>
2023-05-03 01:15:59 +0200

    esto no chuta

--- inicio.py
+++ inicio.py
@@ -1,27 +1,35 @@
-import psutil
+import nmap 
+import json
+# Escaneamos una IP para obtener los puertos abiertos 
+# i los servicios que corren en cada puerto
+def scan_ip(ip):
+    nm = nmap.PortScanner()
+    nm.scan(ip, arguments='-sS -O -v -T4 -p 1-65535')
+# Creamos un diccionario para guardar la información del escaneo
+results = {}
+# Recorremos los puertos y guardamos la información en el diccionario
+for host in nm.all_host():
+    results[host] = {}
+    for port in nm[host]['tcp']:
+        state = nm[host]['tcp'][port]['state']
+        service = nm[host]['tcp'][port]['name']
+        results[host][port] = {'state': state, 'service':service}
+# Guardamos el resultado en un archivo json
+with open('scan_results_json', 'w') as f:
+    json.dump(results, f)
+# Mostramos los resultados por consola y lo guardamos en un archivo txt
+with open('scan_results.txt', 'w') as f:
+    for host in results:
+        print(f'host: {host}\n')
+        f.write(f'host: {host}\n')
+        for port in results[host]:
+            state = results[host][port]['status ']
+            service = results[host][port]['service']
+            print(f' Puerto {port}: {state} {{service}}\n')
+            f.write(f' Puerto {port}: {state} {{service}}\n')
 
-def count_connections(port):
+# Pedimos que se introduzca una IP
+ip = input('Introduce la IP: ')
 
-    # Obtenemos todas la conexiones de red
-    connections = psutil.net_connections()
-
-    # Creamos un diccionario para contar el número de conexiones por puerto
-    port.counts = {}
-
-    # Contamos el número de conexiones recibidas en cada puerto
-    for c in connections:
-        if c.status == 'ESTABLISHED':
-            port = c.laddr.port
-            if port not in port.counts:
-                port_counts[port] = 1
-            else:
-                port_counts[por] += 1
-
-# Mostramos el resultado por puerto
-for port, count in port_counts.items():
-    print(f'El puerto {port} está recibiendo {count} conexiones')
-
-# Contamos el número de conexiones recibidas en todos los puertos
-count_connections()
-
-# Espero que esto chute
+# Escaneamos la IP, mostramos y guardamos
+scan_ip(ip)
