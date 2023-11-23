e17ff7ad816c88e3ffd22de4c1d56f792a14f144
b4ds4ng <andreu.pastor@telefonica.net>
2023-11-23 15:06:12 +0100

    nous arxius

--- init.py
+++ init.py
@@ -1,18 +1,24 @@
 import tkinter as tk
 from  tkinter import ttk
+from gestio import afegir, modificar, eliminar, cerca
 
-# Ventana principal
+
+# finestra login
+
+# finestra principal
 main = tk.Tk()
-main.title('Olga')
+main.title('Gestió')
 main.resizable(0,0)
 main.geometry("1200x650+100+10")
+
+
 
 
 # Menú lateral
 lateral_izquierdo = tk.Frame(main, relief=tk.SOLID, width=200, height=650, padx=10, pady=10, bg='sky blue')
 lateral_izquierdo.pack(side="left", expand=tk.NO)
 
-# parte derecha
+# part dreta, contingut
 lateral_derecho = tk.Frame(main, relief=tk.SOLID, bg="white")
 lateral_derecho.pack(side="right", expand=tk.YES, fill=tk.BOTH)
 
---
+++ modulos/gestio.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+from  tkinter import ttk
+
+
+
+
+class gestio:
+    
+    def afegir():
+        pass
+
+    def modificar():
+        pass
+
+    def eliminar():
+        pass
+
+    def cerca():
+        pass
