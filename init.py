08be61356fdfa87150588c20e4c2bb85f5894bb3
b4ds4ng <andreu.pastor@telefonica.net>
2024-12-06 19:01:57 +0100

    6-12-2024

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 from modulos.principal import Iniciador
-"""Aplicació de gestió de productes esotèrics, amb els clients i els tractaments"""
+"""Aplicació de gestió de productes esotèrics, amb els clients i els tractaments i que amb
+IA poder fer consultes sobre les dades pròpies"""
 
 app = Iniciador()
 app.mainloop()
