b280d83c6678af32d6dc2d1bafcef2b4edf955d6
b4ds4ng <andreu.pastor@telefonica.net>
2024-12-12 18:25:15 +0100

    b4ds4ng.12-12-2024- 18:24h

--- init.py
+++ init.py
@@ -1,6 +1,6 @@
 from modulos.principal import Iniciador
 """Aplicació de gestió de productes esotèrics, amb els clients i els tractaments i que amb
-IA poder fer consultes sobre les dades pròpies"""
+IA poder fer consultes sobre les dades pròpies, provant si git funciona"""
 
 app = Iniciador()
 app.mainloop()
