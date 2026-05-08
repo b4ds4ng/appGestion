f40e65ae4562d6c3a6a0cf5851a52565c45a62e2
b4ds4ng <93019327+b4ds4ng@users.noreply.github.com>
2026-05-09 00:20:02 +0200

    millores

--- init.py
+++ init.py
@@ -1,7 +1,7 @@
 from modulos.principal import Iniciador
 
-"""Aplicació de gestió de productes esotèrics, amb els clients i els tractaments, i que, amb
-IA poder fer consultes sobre les dades pròpies"""
+"""Aplicació de gestió de productes i tractaments d'un petit negoci de teràpies alternatives que amb
+IA es pot fer consultes sobre les dades pròpies."""
 
 app = Iniciador()
 app.mainloop()
